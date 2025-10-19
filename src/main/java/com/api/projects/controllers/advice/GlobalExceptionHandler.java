913c5fb9cd8c6537c13d6339106c67fc0aabb850
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-10-19 00:01:20 -0300

    chore:(TASK-32): import unusable removed

--- src/main/java/com/api/projects/controllers/advice/GlobalExceptionHandler.java
+++ src/main/java/com/api/projects/controllers/advice/GlobalExceptionHandler.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.validation.FieldError;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
